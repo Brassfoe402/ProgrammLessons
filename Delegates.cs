1dbb5d24f68465f0880e02def026623a788779ed
Brassfoe402 <125663981+Brassfoe402@users.noreply.github.com>
2023-05-12 20:57:21 +0600

    Add files via upload

---
+++ Delegates.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Car
+    {
+        public string Make { get; }
+        public string Model { get; }
+
+        public Car(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+    }
+
+    class Garage
+    {
+        private readonly List<Car> cars = new List<Car>();
+
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+        }
+
+        public void WashAllCars()
+        {
+            WashCarDelegate washDelegate = new WashCarDelegate(WashCar);
+            cars.ForEach(car => washDelegate(car));
+        }
+
+        private delegate void WashCarDelegate(Car car);
+
+        private void WashCar(Car car)
+        {
+            Console.WriteLine($"Моем машину {car.Make} {car.Model}");
+        }
+    }
+
+    class CarWash
+    {
+        public static void Main()
+        {
+            var car1 = new Car("Toyota", "Corolla");
+
+            var car2 = new Car("Honda", "Civic");
+
+            var garage = new Garage();
+
+            garage.AddCar(car1);
+
+            garage.AddCar(car2);
+
+            garage.WashAllCars();
+     
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Generalizations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class GenericArray<T>
+{
+    private T[] array;
+
+    public GenericArray(int size)
+    {
+        array = new T[size]; 
+    }
+
+    
+    public void Add(T item, int index)
+    {
+        array[index] = item;
+    }
+
+    
+    public void Remove(int index)
+    {
+        array[index] = default(T); 
+    }
+
+    
+    public T Get(int index)
+    {
+        return array[index];
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        GenericArray<string> stringArray = new GenericArray<string>(5);
+        stringArray.Add("строка1", 0);
+        stringArray.Add("строка2", 1);
+        stringArray.Add("строка3", 2);
+        stringArray.Add("строка4", 3);
+        stringArray.Add("строка5", 4);
+        stringArray.Remove(3);
+        Console.WriteLine(stringArray.Get(2)); 
+
+        
+        GenericArray<int> intArray = new GenericArray<int>(3);
+        intArray.Add(1, 0);
+        intArray.Add(2, 1);
+        intArray.Add(3, 2);
+        Console.WriteLine(intArray.Get(1)); 
+
+        Console.ReadKey();
+    }
+}
+
+
